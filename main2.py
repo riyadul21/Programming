a667f5163c953a8576233282a64ba6d3d45b33af
Tauseef <48177185+Tauseef11@users.noreply.github.com>
2020-12-15 16:04:18 +0000

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,51 @@
+def Main_Menu():
+    userOptions = int(input("Main Menu\n"
+    "1)Add New Address\n"
+    "2)Change Existing User\n"
+    "3)View All Addresses Where Last Name Starts With Letter\n"
+    "4)List All Addresses\n"
+    "5)Quit\n" ))
+
+
+
+    if userOptions == 1:
+         print("1) Add new Address")
+         if userOptions == 1:
+              first_name = input("Enter your first name")
+              last_name = input("Enter your Surname")
+         if first_name + last_name == "":
+           print("No Name Added")
+           Main_Menu()
+         else:
+             address_line1 = input("Enter First Line of Address")
+             address_line2 = input("Enter Second Line of Address")
+             address_line3 = input("Enter Third Line of Address")
+             address_line4 = input("Enter Fourth Line of Address")
+             postcode = input("Enter the PostCode")
+             stdcode = int(input("Enter Area Code for Telephone Number"))
+             telephone_number = int(input("Enter Telephone Number"))
+    elif userOptions == 2:
+         print("2) Change Existing User")
+         last_name_change = input("Enter The Last Name of The Persons Address You Would Like To Change\n")
+         while last_name_change != "Riyadul":
+             print("LastName was NOT found\n")
+             last_name_change = input("Enter The Last Name of The Persons Address You Would Like To Change\n")
+         else:
+            address_line1 = input("Enter First Line of Address\n")
+            address_line2 = input("Enter Second Line of Address\n")
+            address_line3 = input("Enter Third Line of Address\n")
+            address_line4 = input("Enter Fourth Line of Address\n")
+            postcode = input("Enter the PostCode\n")
+            stdcode = int(input("Enter Area Code for Telephone Number\n"))
+            telephone_number = int(input("Enter Telephone Number\n"))
+    elif userOptions == 3:
+         print("3)View All Addresses Where Last Name Starts With Letter")
+    elif userOptions == 4:
+         print("4)List All Addresses\n")
+    elif userOptions == 5:
+         print("5) Quit")
+    else:
+         print("Input not recognised, please try again...")
+
+
+Main_Menu()
