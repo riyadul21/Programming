1dd316c7198f0b6d0a84545697ab058be79f8e45
riyadul21 <36416028+riyadul21@users.noreply.github.com>
2020-12-18 09:19:11 +0000

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -9,7 +9,7 @@
 userList = []
 
 def Main_Menu():
-    
+
     userOptions = int(input("Main Menu\n"
     "1)Add New Address\n"
     "2)Change Existing User\n"
@@ -40,39 +40,40 @@ def Main_Menu():
     elif userOptions == 2:
          print("2) Change Existing User")
          last_name_change = input("Enter The Last Name of The Persons Address You Would Like To Change\n")
+
          i = 0
          amt_tries = len(userList)
          while last_name_change not in userList[i][1]:
                i += 1
-               if i == amt_tries:
-                print("LastName was NOT found\n")
+         if i == amt_tries:
+                print("LastName was NOT found")
                 Main_Menu()
 
-    if last_name_change in userList[i][1]:
-         print("Name found")
-         print("Their first line of their current addreess is", userList[i][2])
-         address_line1pt2 = input("Enter New First Line of Address")
-         userList[i][2] = address_line1pt2
-         print("Their second line of their current addreess is", userList[i][3])
-         address_line2pt2 = input("Enter New Second Line of Address")
-         userList[i][3] = address_line2pt2
-         print("Their third line of their current addreess is", userList[i][4])
-         address_line3pt2 = input("Enter New Fourth Line of Address")
-         userList[i][4] = address_line3pt2
-         print("Their fourth line of their current addreess is", userList[i][5])
-         address_line4pt2 = input("Enter New Fourth Line of Address")
-         userList[i][5] = address_line4pt2
-         print("Their Post Code is", userList[i][6])
-         postcodept2 = input ("Enter New Post code of Address")
-         userList[i][6] = postcodept2
-         print("Their stdcode is", userList[i][7])
-         stdcodept2 = input ("Enter New Area Code")
-         userList[i][7] = stdcodept2
-         print("Telephone Number is", userList[i][8])
-         telephone_numberpt2 = input ("Enter New Telephone Number")
-         userList[i][8] = telephone_numberpt2
-         print("New Address has been updated", userList[i])
-         Main_Menu()
+         if last_name_change in userList[i][1]:
+          print("Name found")
+          print("Their first line of their current addreess is", userList[i][2])
+          address_line1pt2 = input("Enter New First Line of Address")
+          userList[i][2] = address_line1pt2
+          print("Their second line of their current addreess is", userList[i][3])
+          address_line2pt2 = input("Enter New Second Line of Address")
+          userList[i][3] = address_line2pt2
+          print("Their third line of their current addreess is", userList[i][4])
+          address_line3pt2 = input("Enter New Fourth Line of Address")
+          userList[i][4] = address_line3pt2
+          print("Their fourth line of their current addreess is", userList[i][5])
+          address_line4pt2 = input("Enter New Fourth Line of Address")
+          userList[i][5] = address_line4pt2
+          print("Their Post Code is", userList[i][6])
+          postcodept2 = input ("Enter New Post code of Address")
+          userList[i][6] = postcodept2
+          print("Their stdcode is", userList[i][7])
+          stdcodept2 = input ("Enter New Area Code")
+          userList[i][7] = stdcodept2
+          print("Telephone Number is", userList[i][8])
+          telephone_numberpt2 = input ("Enter New Telephone Number")
+          userList[i][8] = telephone_numberpt2
+          print("New Address has been updated", userList[i])
+          Main_Menu()
 
 
 
@@ -80,20 +81,20 @@ def Main_Menu():
     elif userOptions == 3:
          print("3)View all addresses where last name starts with letter")
 
-         LastLetter = input ("Enter the first letter of their last name")
+         last_letter = input ("Enter the first letter of their last name")
 
          i = 0
          amt_tries = len(userList)
-         while lastLetter not in userList[i][1][0]:
+         while last_letter not in userList[i][1][0]:
                i += 1
                if i == amt_tries:
                   print("Last name not found!")
                   Main_Menu
-         if LastLetter in userList[i][1][0]:
+         if last_letter in userList[i][1][0]:
             print("Last name has been found")
-            print("\nfirstname", userList[i][0], "\nlastname", userList[i][1], "\naddress", userList[i][2],
-            "\n", userList[i][3], "\n", userList[i][4], "\n", userList[i][5], "\nPostcode", userList[i][6],
-            "\nstdcode", userList[i][7], "\ntelephone", userList[i][8], "\n" )
+            print("\nFirst name", userList[i][0], "\nLast name", userList[i][1], "\nAddress", userList[i][2],
+            "\n", userList[i][3], "\n", userList[i][4], "\n", userList[i][5], "\nPost Code", userList[i][6],
+            "\nStdcode", userList[i][7], "\nTelephone", userList[i][8], "\n" )
             Main_Menu
 
 
